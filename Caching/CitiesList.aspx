30ab93b0d0483623f66338c163e9ef1423517239
tonousa@hotmail.com <tonousa@hotmail.com>
2019-10-15 13:31:42 -0500

    output cache dependencies

---
+++ Caching/CitiesList.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CitiesList.aspx.cs" Inherits="Caching.CitiesList" %>
+
+<%@ OutputCache Duration="60" VaryByParam="none" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    Here are some cities: 
+    <%= GetCities() %>
+    (Rendered at <%: DateTime.Now.ToLongTimeString() %>)
+</body>
+</html>
---
+++ Caching/CitiesList.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Caching
+{
+    public partial class CitiesList : System.Web.UI.Page
+    {
+        private static readonly string filename = "/CitiesList.html";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.AddFileDependency(MapPath(filename));
+        }
+
+        protected string GetCities()
+        {
+            return File.ReadAllText(MapPath(filename));
+        }
+    }
+}

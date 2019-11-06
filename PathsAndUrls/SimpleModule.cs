63990babd91e8978cffb595c2e251a1bfce43e76
tonousa@hotmail.com <tonousa@hotmail.com>
2019-11-06 13:04:06 -0600

    578

---
+++ PathsAndUrls/SimpleModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace PathsAndUrls
+{
+    public class SimpleModule : IHttpModule
+    {
+        public void Init(HttpApplication app)
+        {
+            app.BeginRequest += (src, args) => ProcessRequest(app);
+        }
+
+        private void ProcessRequest(HttpApplication app)
+        {
+            WriteMsg("URL requested {0}", app.Request.RawUrl);
+        }
+
+        private void WriteMsg(string formatString, params object[] vals)
+        {
+            Debug.WriteLine(formatString, vals);
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}

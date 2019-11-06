941fcade05a53b649a873762e44caf39a1b2f8f8
tonousa@hotmail.com <tonousa@hotmail.com>
2019-11-06 16:44:21 -0600

    586

---
+++ PathsAndUrls/ExtensionlessHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace PathsAndUrls
+{
+    public class ExtensionlessHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("<p>Extensionless Handler</p>");
+            string vpath = context.Request.Path;
+            if (vpath == "/")
+            {
+                context.Server.Transfer("/Default.aspx");
+            }
+            else if (File.Exists(context.Request.MapPath(vpath + ".aspx")))
+            {
+                context.Server.Transfer(vpath + ".aspx");
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+                context.ApplicationInstance.CompleteRequest();
+            }
+        }
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+    }
+}

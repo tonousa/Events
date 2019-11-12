516a6444ad11247e2c58bb0b037d236c119b03e8
tonousa <tonousa@hotmail.com>
2019-11-11 20:00:52 -0600

    613 fixed routes

---
+++ Routing/App_Start/RouteConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace Routing
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute("default", "", "~/Default.aspx");
+            routes.MapPageRoute("cart1", "cart", "~/Store/Cart.aspx");
+            routes.MapPageRoute("cart2", "apps/shopping/finish", "~/Store/Cart.aspx");
+        }
+    }
+}

346fd8aa6898b164fa7c266fc84fcc6ee77d794c
tonousa <tonousa@hotmail.com>
2019-11-13 23:54:05 -0600

    616 variable segments

--- Routing/App_Start/RouteConfig.cs
+++ Routing/App_Start/RouteConfig.cs
@@ -13,6 +13,12 @@ namespace Routing
             routes.MapPageRoute("default", "", "~/Default.aspx");
             routes.MapPageRoute("cart1", "cart", "~/Store/Cart.aspx");
             routes.MapPageRoute("cart2", "apps/shopping/finish", "~/Store/Cart.aspx");
+
+            routes.MapPageRoute("dall", "{app}/default", "~/Default.aspx");
+            //routes.MapPageRoute("d2", "accounts/default", "~/Default.aspx");
+            //routes.MapPageRoute("d3", "payments/default", "~/Default.aspx");
+            routes.MapPageRoute("d4", "store/default", "~/Store/Cart.aspx");
+
         }
     }
 }

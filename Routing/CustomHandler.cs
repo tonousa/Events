2cf6c9e3d0213147867620f33a9906c7105e1774
tonousa@hotmail.com <tonousa@hotmail.com>
2019-11-26 17:12:32 -0600

    660 routing other files

---
+++ Routing/CustomHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Routing
+{
+    public class CustomHandlerFactory : IHttpHandlerFactory
+    {
+        public IHttpHandler GetHandler(HttpContext context, string requestType,
+            string url, string pathTranslated)
+        {
+            return new CustomHandler() { FactoryCreated = true };
+        }
+
+        public void ReleaseHandler(IHttpHandler handler)
+        {
+            //
+        }
+    }
+
+    public class CustomHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("This is the custom hanlder");
+            if (FactoryCreated)
+            {
+                context.Response.Write(" (Created via the Factory)");
+            }
+            else
+            {
+                context.Response.Write(" (Created directly)");
+            }
+        }
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public bool FactoryCreated { get; set; }
+    }
+}
---
+++ Routing/GenHandler.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Routing
+{
+    /// <summary>
+    /// Summary description for GenHandler
+    /// </summary>
+    public class GenHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World from Generic Handler");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}

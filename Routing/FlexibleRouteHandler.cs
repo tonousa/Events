10a501f7a6fb1f8a8e7a9e7482521ecc27369e64
tonousa@hotmail.com <tonousa@hotmail.com>
2019-11-27 11:55:52 -0600

    661

---
+++ Routing/FlexibleRouteHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace Routing
+{
+    public class FlexibleRouteHandler : IRouteHandler
+    {
+        public string HandlerType { get; set; }
+
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            IHttpHandler handler = null;
+
+            if (HandlerType != null)
+            {
+                object target = Activator.CreateInstance(Type.GetType(HandlerType));
+                if (target is IHttpHandlerFactory 
+                    && requestContext.HttpContext is HttpContextWrapper)
+                {
+                    handler = (target as IHttpHandlerFactory)
+                }
+            }
+        }
+    }
+}

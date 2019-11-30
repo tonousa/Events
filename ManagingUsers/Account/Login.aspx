b84d5999b4e2d904c01223bd69b03d2f9f92d6da
tonousa <tonousa@hotmail.com>
2019-11-30 17:53:54 -0600

    674 login form

---
+++ ManagingUsers/Account/Login.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="ManagingUsers.Account.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <div class="details">The request is authenticated: <%: GetRequestStatus() %></div>
+    <div class="details">The current user is: <%: GetUser() %></div>
+    <form id="form1" runat="server">
+        <div><label>User:</label><input name="user" /></div>
+        <div><label>Password:</label><input name="pass" type="password" /></div>
+        <div>
+            <button name="action" value="login" type="submit">Log In</button>
+            <button name="action" value="logout" type="submit">Log Out</button>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ManagingUsers/Account/Login.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace ManagingUsers.Account
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                string user = Request["user"];
+                string pass = Request["pass"];
+                string action = Request["action"];
+                if (action == "login" && user == "Joe" && pass == "secret")
+                {
+                    FormsAuthentication.SetAuthCookie(user, false);
+                }
+                else
+                {
+                    FormsAuthentication.SignOut();
+                }
+                Response.Redirect(Request.Path);
+            }
+        }
+
+        protected bool GetRequestStatus()
+        {
+            return Request.IsAuthenticated;
+        }
+        
+        protected string GetUser()
+        {
+            return Context.User.Identity.Name;
+        }
+    }
+}

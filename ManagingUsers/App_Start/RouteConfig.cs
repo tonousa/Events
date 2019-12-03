8afbcad0124a00845740cb334d9584a2b3eab967
tonousa@hotmail.com <tonousa@hotmail.com>
2019-12-03 17:49:10 -0600

    691 Authorize Routed URLS

--- ManagingUsers/App_Start/RouteConfig.cs
+++ ManagingUsers/App_Start/RouteConfig.cs
@@ -10,7 +10,8 @@ namespace ManagingUsers
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-
+            routes.MapPageRoute(null, "", "~/Default.aspx", true);
+            routes.MapPageRoute(null, "restricted", "~/Admin/Restricted.aspx", true);
         }
     }
 }

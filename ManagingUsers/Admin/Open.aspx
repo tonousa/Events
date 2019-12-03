293372f3cbe50e1cbc8298c6349a0a0957b7a480
tonousa@hotmail.com <tonousa@hotmail.com>
2019-12-03 17:37:15 -0600

    689  Bypassing Authorization app.PostAuthenticateRequest

---
+++ ManagingUsers/Admin/Open.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Open.aspx.cs" Inherits="ManagingUsers.Admin.Open" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <p>This is /Admin/Open.aspx</p>
+</body>
+</html>
---
+++ ManagingUsers/AuthModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManagingUsers
+{
+    public class AuthModule : IHttpModule
+    {
+        public void Init(HttpApplication app)
+        {
+            app.PostAuthenticateRequest += (src, args) =>
+            {
+                if (app.Request.Path == "/Admin/Open.aspx")
+                {
+                    app.Context.SkipAuthorization = true;
+                }
+            };
+        }
+
+        public void Dispose()
+        {
+            //
+        }
+    }
+}

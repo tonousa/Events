d5df4fa3ffa3db7ca2eb1cd63199b18ffb2f318c
tonousa@hotmail.com <tonousa@hotmail.com>
2019-12-06 16:54:39 -0600

    714 Password Recovery MembershipUser.ResetPassword(answer)

---
+++ ManagingUsers/Account/Recover.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ManagingUsers.Account
+{
+    public partial class Recover : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                if (task.Value == "Next")
+                {
+                    MembershipUser mUser = Membership.GetUser(Request["user"]);
+                    if (mUser != null)
+                    {
+                        question.Visible = true;
+                        questionLabel.InnerText = mUser.PasswordQuestion;
+                        if (Request["answer"] != null)
+                        {
+                            try
+                            {
+                                string newPassword =
+                                    mUser.ResetPassword(Request["answer"]);
+                                username.Visible = false;
+                                question.Visible = false;
+                                newpass.Visible = true;
+                                password.InnerText = newPassword;
+                                task.Value = "Log In";
+                            }
+                            catch (Exception)
+                            {
+                                ReportError("Wrong answer");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        ReportError("Unknown username");
+                    }
+                }
+                else if (task.Value == "Restart")
+                {
+                    Response.Redirect(Request.Path);
+                }
+                else
+                {
+                    Response.Redirect(FormsAuthentication.LoginUrl);
+                }
+            }
+        }
+
+        private void ReportError(string errorMsg)
+        {
+            message.InnerText = "Error: " + errorMsg;
+            error.Visible = true;
+            task.Value = "Restart";
+        }
+    }
+}

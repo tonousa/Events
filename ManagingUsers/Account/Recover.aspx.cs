2a9c688a00aa62f27ed910cd2a661190f2373ca1
tonousa@hotmail.com <tonousa@hotmail.com>
2019-12-10 15:55:05 -0600

    715 pass recovery MembershipUser.ResetPassword(answer)

--- ManagingUsers/Account/Recover.aspx.cs
+++ ManagingUsers/Account/Recover.aspx.cs
@@ -27,11 +27,14 @@ namespace ManagingUsers.Account
                             {
                                 string newPassword =
                                     mUser.ResetPassword(Request["answer"]);
-                                username.Visible = false;
-                                question.Visible = false;
-                                newpass.Visible = true;
-                                password.InnerText = newPassword;
-                                task.Value = "Log In";
+                                Session["oldpass"] = newPassword;
+                                FormsAuthentication.SetAuthCookie(mUser.UserName, false);
+                                Response.Redirect("/Account/Change.aspx");
+                                //username.Visible = false;
+                                //question.Visible = false;
+                                //newpass.Visible = true;
+                                //password.InnerText = newPassword;
+                                //task.Value = "Log In";
                             }
                             catch (Exception)
                             {

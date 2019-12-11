3adb46afc50fb30eaa4b13e7bc11693822e6e0f0
tonousa@hotmail.com <tonousa@hotmail.com>
2019-12-11 11:44:24 -0600

    718 register user Membership.CreateUser(name, pass...

--- ManagingUsers/Account/Register.aspx.cs
+++ ManagingUsers/Account/Register.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,7 +12,42 @@ namespace ManagingUsers.Account
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack)
+            {
+                string username = Request["user"];
+                string password = Request["pass"];
+                string email = Request["email"];
+                string question = Request["question"];
+                string answer = Request["answer"];
 
+                if (username == "" || password == "" || email == "" || answer == "")
+                {
+                    ReportError("All fields must be filled");
+                }
+                else
+                {
+                    MembershipCreateStatus status;
+                    MembershipUser user = Membership.CreateUser(username, password,
+                        email, question, answer, true, out status);
+
+                    if (status == MembershipCreateStatus.Success)
+                    {
+                        Roles.AddUserToRole(username, "users");
+                        FormsAuthentication.SetAuthCookie(username, false);
+                        Response.Redirect("/");
+                    }
+                    else
+                    {
+                        ReportError(status.ToString());
+                    }
+                }
+            }
+        }
+
+        private void ReportError(string errorMsg)
+        {
+            message.InnerText = "Error: " + errorMsg;
+            error.Visible = true;
         }
     }
 }

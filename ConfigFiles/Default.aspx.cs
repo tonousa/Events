385689362922914c1e965aea89b902415751fbf2
tonousa@hotmail.com <tonousa@hotmail.com>
2019-12-13 15:11:12 -0600

    736  WebConfigurationManager.ConnectionStrings[name]

--- ConfigFiles/Default.aspx.cs
+++ ConfigFiles/Default.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
 using System.Linq;
 using System.Web;
 using System.Web.Configuration;
@@ -17,11 +19,41 @@ namespace ConfigFiles
 
         public IEnumerable<string> GetConfig()
         {
-            foreach (string key in WebConfigurationManager.AppSettings)
+            //foreach (string key in WebConfigurationManager.AppSettings)
+            //{
+            //    yield return string.Format("{0} = {1}",
+            //        key, WebConfigurationManager.AppSettings[key]);
+            //}
+
+            //foreach (ConnectionStringSettings con in 
+            //    WebConfigurationManager.ConnectionStrings)
+            //{
+            //    yield return string.Format("name: {0}, connectionString: {1}",
+            //        con.Name, con.ConnectionString);
+            //}
+
+            string csName = WebConfigurationManager.AppSettings["dbConnectionString"];
+
+            ConnectionStringSettings conString =
+                WebConfigurationManager.ConnectionStrings[csName];
+
+            DbConnection dbConn = DbProviderFactories
+                .GetFactory(conString.ProviderName).CreateConnection();
+
+            dbConn.ConnectionString = conString.ConnectionString;
+            dbConn.Open();
+
+            DbCommand dbCommand = dbConn.CreateCommand();
+            dbCommand.CommandText = "SELECT UserName from Users";
+            dbCommand.CommandType = System.Data.CommandType.Text;
+
+            DbDataReader reader = dbCommand.ExecuteReader();
+            while (reader.Read())
             {
-                yield return string.Format("{0} = {1}",
-                    key, WebConfigurationManager.AppSettings[key]);
+                yield return reader[0].ToString();
             }
+
+            dbConn.Close();
         }
     }
 }

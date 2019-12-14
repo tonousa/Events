c0ecb9c507c9ef079b1ecbcb0152daa2079ba8dc
tonousa <tonousa@hotmail.com>
2019-12-14 12:15:36 -0600

    743 WebConfigurationManager.OpenWebConfiguration(Request.Path)

--- ConfigFiles/Default.aspx.cs
+++ ConfigFiles/Default.aspx.cs
@@ -32,28 +32,75 @@ namespace ConfigFiles
             //        con.Name, con.ConnectionString);
             //}
 
-            string csName = WebConfigurationManager.AppSettings["dbConnectionString"];
+            //string csName = WebConfigurationManager.AppSettings["dbConnectionString"];
 
-            ConnectionStringSettings conString =
-                WebConfigurationManager.ConnectionStrings[csName];
+            //ConnectionStringSettings conString =
+            //    WebConfigurationManager.ConnectionStrings[csName];
 
-            DbConnection dbConn = DbProviderFactories
-                .GetFactory(conString.ProviderName).CreateConnection();
+            //DbConnection dbConn = DbProviderFactories
+            //    .GetFactory(conString.ProviderName).CreateConnection();
 
-            dbConn.ConnectionString = conString.ConnectionString;
-            dbConn.Open();
+            //dbConn.ConnectionString = conString.ConnectionString;
+            //dbConn.Open();
 
-            DbCommand dbCommand = dbConn.CreateCommand();
-            dbCommand.CommandText = "SELECT UserName from Users";
-            dbCommand.CommandType = System.Data.CommandType.Text;
+            //DbCommand dbCommand = dbConn.CreateCommand();
+            //dbCommand.CommandText = "SELECT UserName from Users";
+            //dbCommand.CommandType = System.Data.CommandType.Text;
 
-            DbDataReader reader = dbCommand.ExecuteReader();
-            while (reader.Read())
+            //DbDataReader reader = dbCommand.ExecuteReader();
+            //while (reader.Read())
+            //{
+            //    yield return reader[0].ToString();
+            //}
+
+            //dbConn.Close();
+
+            //object configObject = WebConfigurationManager
+            //    .GetWebApplicationSection("system.web/compilation");
+            //CompilationSection sectionHandler = configObject as CompilationSection;
+            //if (sectionHandler != null)
+            //{
+            //    yield return string.Format("debug = {0}", sectionHandler.Debug);
+            //    yield return string.Format("targetFramework = {0}", sectionHandler.TargetFramework);
+            //    yield return string.Format("batch = {0}", sectionHandler.Batch);
+
+            //}
+            //else
+            //{
+            //    yield return string.Format("unexpected object type = {0}",
+            //        sectionHandler.GetType());
+
+            //}
+
+            Configuration config = WebConfigurationManager.OpenWebConfiguration(Request.Path);
+
+            foreach (ConfigurationSectionGroup group in config.SectionGroups)
             {
-                yield return reader[0].ToString();
+                foreach (string str in processSectionGroup(group))
+                {
+                    yield return str;
+                }
             }
 
-            dbConn.Close();
+            //SystemWebSectionGroup group =
+            //    (SystemWebSectionGroup)config.SectionGroups["system.web"];
+            //CompilationSection section = group.Compilation;
+            //yield return string.Format("debug = {0}", group.Compilation.Debug);
+            //yield return string.Format("targetframework = {0}", group.Compilation.TargetFramework);
+            //yield return string.Format("batch = {0}", group.Compilation.Batch);
+        }
+
+        private IEnumerable<string> processSectionGroup(ConfigurationSectionGroup group)
+        {
+            yield return string.Format("<b>Section group: {0}</b>", group.Name);
+            foreach (ConfigurationSectionGroup innerGroup in group.SectionGroups)
+            {
+                processSectionGroup(innerGroup);
+            }
+            foreach (ConfigurationSection section in group.Sections)
+            {
+                yield return string.Format("Section: {0}", section.SectionInformation.Name);
+            }
         }
     }
 }

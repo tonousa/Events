bc9a798447fbd9389935e3fcffd00ff55ff3a3c2
tonousa@hotmail.com <tonousa@hotmail.com>
2019-12-18 11:12:27 -0600

    776 HttpTaskAsyncHandler ProcessRequestAsync

---
+++ AsyncApp/AsyncHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AsyncApp
+{
+    public class AsyncHandler : HttpTaskAsyncHandler
+    {
+        public override async Task ProcessRequestAsync(HttpContext context)
+        {
+            string webResponse
+                = await new WebClient().DownloadStringTaskAsync("http://ebay.com");
+            context.Response.Write(string.Format("Length of result: {0}",
+                webResponse.Length));
+        }
+    }
+}

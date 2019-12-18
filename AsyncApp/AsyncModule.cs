35628b80cc2a580cde45bc83097bf15821904cb5
tonousa@hotmail.com <tonousa@hotmail.com>
2019-12-18 10:15:00 -0600

    774 Async Module, EventHandlerTaskAsyncHelper

---
+++ AsyncApp/AsyncModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace AsyncApp
+{
+    public class AsyncModule : IHttpModule
+    {
+        public void Init(HttpApplication app)
+        {
+            EventHandlerTaskAsyncHelper helper
+                = new EventHandlerTaskAsyncHelper(async (src, args) =>
+                {
+                    if (app.Context.Request.Path == "/DisplayItemValue.aspx")
+                    {
+                        string content = await new
+                            WebClient().DownloadStringTaskAsync("http://ebay.com");
+                        ((HttpApplication)src).Context.Items["length"] = content.Length;
+                    }
+                });
+            app.AddOnBeginRequestAsync(helper.BeginEventHandler, helper.EndEventHandler);
+        }
+
+        public void Dispose()
+        {
+            //
+        }
+    }
+}
---
+++ AsyncApp/DisplayItemValue.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DisplayItemValue.aspx.cs" Inherits="AsyncApp.DisplayItemValue" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        The length of the result is: <%: Context.Items["length"] %>
+    </div>
+    </form>
+</body>
+</html>

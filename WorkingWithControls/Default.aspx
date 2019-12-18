24f901c4ac0007e56c97d31a9d84d3393967f6e9
tonousa@hotmail.com <tonousa@hotmail.com>
2019-12-18 12:37:11 -0600

    781  all controls derived from System.Web.UI.Control

---
+++ WorkingWithControls/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WorkingWithControls.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style> div { margin-top: 10px;} </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Button presses: <span id="counter" runat="server"></span>
+    </div>
+    <div>
+        <button type="submit">Submit</button>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WorkingWithControls/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WorkingWithControls
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int countVal = (int)(Session["counter"] ?? 0);
+            if (IsPostBack)
+            {
+                Session["counter"] = ++countVal;
+            }
+            counter.InnerText = countVal.ToString();
+        }
+    }
+}

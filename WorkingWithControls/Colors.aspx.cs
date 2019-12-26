fdc5965b0c54b14c95e85eed939ca2fa464fa59f
tonousa <tonousa@hotmail.com>
2019-12-25 20:02:33 -0600

    807 Controls.Add(

---
+++ WorkingWithControls/Colors.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace WorkingWithControls
+{
+    public partial class Colors : System.Web.UI.Page
+    {
+        private string[] colors = { "Red", "Green", "Blue"};
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HtmlGenericControl div = FindControl("buttonTarget") as HtmlGenericControl;
+            foreach (string text in colors)
+            {
+                Button b = new Button();
+                b.Text = text;
+                b.EnableViewState = false;
+                div.Controls.Add(b);
+            }
+            ControlUtils.AddButtonClickHandlers(this);
+        }
+    }
+}

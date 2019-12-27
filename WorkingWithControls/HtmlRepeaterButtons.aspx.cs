e29b65b86c81bca5f8d5c89a16c1a2090dd3220d
tonousa@hotmail.com <tonousa@hotmail.com>
2019-12-27 09:31:46 -0600

    812 Request.Form["something"]

---
+++ WorkingWithControls/HtmlRepeaterButtons.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WorkingWithControls
+{
+    public partial class HtmlRepeaterButtons : System.Web.UI.Page
+    {
+        private string[] colors = { "Red", "Green", "Blue" };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack && Request.Form["action"] != null)
+            {
+                selectedValue.InnerText = string.Format("The {0} button was clicked",
+                    Request.Form["action"]);
+            }
+        }
+
+        public IEnumerable<string> GetButtonDetails()
+        {
+            return colors;
+        }
+    }
+}

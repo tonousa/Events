31822927299d99ae47291b3ae85e88986cc14716
tonousa@hotmail.com <tonousa@hotmail.com>
2020-01-03 16:20:13 -0600

    866 add controls programmatically Page.LoadControl(*.ascx)

---
+++ Controls/Loader.aspx.cs
@@ -0,0 +1,27 @@
+﻿using Controls.Custom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Controls
+{
+    public partial class Loader : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BasicCalc calc = (BasicCalc)LoadControl("~/Custom/BasicCalc.ascx");
+            calc.Initial = 500;
+
+            List<Calculation> calcs = new List<Calculation>
+            {
+                new Calculation { Value = 90, Operation = BasicCalc.OperationType.Plus },
+                new Calculation { Value = 50, Operation = BasicCalc.OperationType.Plus }
+            };
+            calc.Calculations = calcs;
+            controlTarget.Controls.Add(calc);
+        }
+    }
+}

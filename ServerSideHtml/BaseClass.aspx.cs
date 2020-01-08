29b7668c712787aa07cdd46915f3d7e138bbfd11
tonousa@hotmail.com <tonousa@hotmail.com>
2020-01-08 11:32:25 -0600

    906 HtmlContainerControl, LiteralControl

---
+++ ServerSideHtml/BaseClass.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerSideHtml
+{
+    public partial class BaseClass : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            userInput.Attributes["class"] = "user";
+
+            foreach (string key in userInput.Attributes.Keys)
+            {
+                Debug.WriteLine("Attribute {0}: {1}", key, userInput.Attributes[key]);
+            }
+
+            Debug.WriteLine(string.Format("tag name: {0}", userInput.TagName));
+        }
+    }
+}
---
+++ ServerSideHtml/Container.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace ServerSideHtml
+{
+    public partial class Container : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ProcessContainerControl(outerDiv);
+        }
+
+        protected void ProcessContainerControl(HtmlContainerControl c)
+        {
+            bool isLiteral = IsLiteralContent(c);
+            Debug.WriteLine("ID: {0} Literal: {1}, InnerText: {2}",
+                c.ID, isLiteral, isLiteral ? c.InnerText.Trim() : "N/A");
+
+            foreach (Control child in c.Controls)
+            {
+                if (child is HtmlContainerControl)
+                {
+                    ProcessContainerControl(child as HtmlContainerControl);
+                }
+            }
+        }
+
+        protected bool IsLiteralContent(Control c)
+        {
+            return c.Controls != null && c.Controls.Count == 1
+                && c.Controls[0] is LiteralControl;
+        }
+    }
+}

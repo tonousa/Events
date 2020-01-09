f78412bdee5f778e18029b552122c9a87f7cd61c
tonousa@hotmail.com <tonousa@hotmail.com>
2020-01-09 15:50:33 -0600

    939 Model Validation system.componentmodel.dataannotations

--- Binding/Models/Person.cs
+++ Binding/Models/Person.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Binding.Models
 {
     public class Person
     {
+        [Required(ErrorMessage = "Enter name")]
+        [StringLength(20, MinimumLength =3, ErrorMessage ="3-20 chars")]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage ="only alpha chars")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage ="enter age")]
+        [Range(5,100, ErrorMessage ="only 5-100")]
         public int Age { get; set; }
+
         public string Cell { get; set; }
+
         public string Zip { get; set; }
     }
 }

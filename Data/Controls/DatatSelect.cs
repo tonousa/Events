9f5c4ec4932273163903f606deb9965113b360c0
tonousa@hotmail.com <tonousa@hotmail.com>
2020-01-13 11:22:09 -0600

    974 DataBoundControl is data control base class

--- Data/Controls/DatatSelect.cs
+++ Data/Controls/DatatSelect.cs
@@ -1,13 +1,59 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace Data.Controls
 {
     public class DatatSelect : DataBoundControl
     {
+        private string[] dataArray;
 
+        public DatatSelect()
+        {
+            Init += (src, args) =>
+            {
+                ViewStateMode = System.Web.UI.ViewStateMode.Disabled;
+            };
+        }
+
+        public string Value
+        {
+            get
+            {
+                return Context.Request.Form[GetId("customSelect")];
+            }
+        }
+
+        protected override void PerformDataBinding(IEnumerable data)
+        {
+            dataArray = data.Cast<string>().ToArray();
+        }
+
+        protected override void RenderContents(HtmlTextWriter writer)
+        {
+            writer.AddAttribute(HtmlTextWriterAttribute.Name, GetId("customSelect"));
+            writer.RenderBeginTag(HtmlTextWriterTag.Select);
+            for (int i = 0; i < dataArray.Length; i++)
+            {
+                writer.AddAttribute(HtmlTextWriterAttribute.Value, dataArray[i]);
+                if (i == 0 && Value == null || dataArray[i] == Value)
+                {
+                    writer.AddAttribute(HtmlTextWriterAttribute.Selected, "selected");
+                }
+                writer.RenderBeginTag(HtmlTextWriterTag.Option);
+                writer.Write(dataArray[i]);
+                writer.RenderEndTag();
+            }
+            writer.RenderEndTag();
+        }
+
+        private string GetId(string name)
+        {
+            return string.Format("{0}{1}{2}", ClientID, ClientIDSeparator, name);
+        }
     }
 }

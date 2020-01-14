6219f2c37c6f762b29e4c7588da5567e3679c84e
tonousa@hotmail.com <tonousa@hotmail.com>
2020-01-14 10:01:52 -0600

    999 ListControl, ListItem classes

---
+++ Data/Check.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Check.aspx.cs" Inherits="Data.Check" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style> div { margin-bottom: 10px; } </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:CheckBoxList runat="server" ID="cbl" AppendDataBoundItems="true"
+             SelectMethod="GetProducts" RepeatColumns="3">
+            <asp:ListItem Text="All" Selected="True" />
+        </asp:CheckBoxList>
+    </div>
+        <div>
+            Selection: <span runat="server" id="selection" />
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
---
+++ Data/Check.aspx.cs
@@ -0,0 +1,34 @@
+﻿using Data.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Data
+{
+    public partial class Check : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                List<string> selected = new List<string>();
+                foreach (ListItem item in cbl.Items)
+                {
+                    if (item.Selected)
+                    {
+                        selected.Add(item.Value);
+                    }
+                }
+                selection.InnerText = string.Join(", ", selected.ToArray());
+            }
+        }
+
+        public IEnumerable<string> GetProducts()
+        {
+            return new Repository().Products.Select(p => p.Name);
+        }
+    }
+}

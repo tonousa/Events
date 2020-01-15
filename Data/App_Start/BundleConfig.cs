e7b54a7e0f157f5238d77aaf92cce380bfd41992
tonousa@hotmail.com <tonousa@hotmail.com>
2020-01-15 11:44:01 -0600

    1022 App_Start RegisterBundles

---
+++ Data/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.UI;
+
+namespace Data
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js"));
+
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery",
+                new ScriptResourceDefinition { Path = "~/bundles/jquery" });
+        }
+    }
+}

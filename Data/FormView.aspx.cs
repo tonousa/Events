6d304ebd95b1ec7db16868823965a2c248bfba8b
tonousa@hotmail.com <tonousa@hotmail.com>
2020-01-16 10:20:22 -0600

    1039 FormView: InsertMethod, UpdateMethod,...

--- Data/FormView.aspx.cs
+++ Data/FormView.aspx.cs
@@ -21,8 +21,32 @@ namespace Data
             return new Repository().Products.AsQueryable<Product>();
         }
 
-        public void UpdateProduct() { }
-        public void DeleteProduct() { }
-        public void InsertProduct() { }
+        public void UpdateProduct(int? productID) {
+            Repository repo = new Repository();
+            Product product = repo.Products
+                .Where(p => p.ProductID == productID).FirstOrDefault();
+            if (product != null && TryUpdateModel<Product>(product))
+            {
+                repo.SaveProduct(product);
+            }
+        }
+
+        public void DeleteProduct(int? productID) {
+            Repository repo = new Repository();
+            Product product = repo.Products
+                .Where(p => p.ProductID == productID).FirstOrDefault();
+            if (product != null)
+            {
+                repo.DeleteProduct(product);
+            }
+        }
+
+        public void InsertProduct() {
+            Product product = new Product();
+            if (TryUpdateModel<Product>(product))
+            {
+                new Repository().AddProduct(product);
+            }
+        }
     }
 }

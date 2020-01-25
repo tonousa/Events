81b173f69ed39d328014f851d3b89961b27395b1
tonousa <tonousa@hotmail.com>
2020-01-25 12:56:00 -0600

    1112 View Model class

--- ClientDev/ProductController.cs
+++ ClientDev/ProductController.cs
@@ -9,31 +9,70 @@ using System.Web.Http;
 
 namespace ClientDev
 {
+    public class ProductView
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+        public ProductView() { }
+
+        public ProductView(Product product)
+        {
+            this.ProductID = product.ProductID;
+            this.Name = product.Name;
+            this.Price = product.Price;
+            this.Category = product.Category;
+        }
+
+        public Product ToProduct()
+        {
+            return new Product
+            {
+                ProductID = this.ProductID,
+                Name = this.Name,
+                Price = this.Price,
+                Category = this.Category
+            };
+        }
+    }
+
     public class ProductController : ApiController
     {
         // GET api/<controller>
-        public IEnumerable<Product> Get()
+        public IEnumerable<ProductView> Get()
         {
-            return new Repository().Products;
+            return new Repository().Products
+                .Select(p => new ProductView(p));
         }
 
         // GET api/<controller>/5
-        public Product Get(int id)
+        public ProductView Get(int id)
         {
             return new Repository().Products
-                .Where(p => p.ProductID == id).FirstOrDefault();
+                .Where(p => p.ProductID == id)
+                .Select(p => new ProductView(p)).FirstOrDefault();
         }
 
         // POST api/<controller>
-        public void Post([FromBody] Product value)
+        public void Post([FromBody] ProductView value)
         {
-            new Repository().SaveProduct(value);
+            new Repository().SaveProduct(value.ToProduct());
         }
 
         // PUT api/<controller>/5
-        public void Put(int id, [FromBody] Product value)
+        public void Put(int id, [FromBody] ProductView value)
         {
-            new Repository().SaveProduct(value);
+            Repository repo = new Repository();
+            Product current = repo.Products
+                .Where(p => p.ProductID == id).FirstOrDefault();
+            if (current != null)
+            {
+                current.Name = value.Name;
+                current.Price = value.Price;
+                current.Category = value.Category;
+            }
         }
 
         // DELETE api/<controller>/5

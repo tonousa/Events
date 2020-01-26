98775836768784d7d12c1e38d057d8bb532b1d90
tonousa <tonousa@hotmail.com>
2020-01-26 16:55:14 -0600

    1116 $.ajax({ url: aaa, type: "POST", success:

---
+++ ClientDev/Data.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Data.aspx.cs" Inherits="ClientDev.Data" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <%: System.Web.Optimization.Scripts.Render("~/bundle/jquery") %>
+    <script src="Scripts/data.js" ></script>
+    <script type="text/template" id="rowTemplate">
+        <tr>
+            <td>{ProductID}</td>
+            <td><input type="text" name="Name" value="{Name}" /></td>
+            <td><input type="text" name="Category" value="{Category}" /></td>
+            <td><input type="text" name="Price" value="{Price}" /></td>
+            <td>
+                <button data-id="{ProductID}" data-action="update">Update</button>
+                <button data-id="{ProductID}" data-action="delete">Delete</button>
+            </td>
+        </tr>
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table id="dataTable">
+            <thead>
+                <tr>
+                    <th>ID</th><th>Name</th><th>Category</th><th>Price</th><th></th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </form>
+</body>
+</html>
---
+++ ClientDev/Scripts/Data.js
@@ -0,0 +1,70 @@
+﻿(function () {
+    String.prototype.format = function (dataObject) {
+        console.log("in format");
+        return this.replace(/{(.+)}/g, function (match, propertyName) {
+            return dataObject[propertyName];
+
+        });
+    };
+
+    function getData() {
+        $.getJSON("/api/product", null, displayData);
+        console.log("in getData");
+    }
+
+    function displayData(data) {
+        var target = $("#dataTable tbody");
+        target.empty();
+        var template = $("#rowTemplate");
+        data.forEach(function (dataObject) {
+            target.append(template.html().format(dataObject));
+        });
+        $(target).find("button").click(function (e) {
+            $("*.errorMsg").remove();
+            $("*.error").removeClass("error");
+            var index = $(e.target).attr("data-id"); // prod id
+            if ($(e.target).attr("data-action") == "delete") {
+                deleteData(index);
+            } else { // update
+                var productData = { productID: index };
+                $(e.target).closest('tr').find('input')
+                    .each(function (index, inputElem) {
+                        productData[inputElem.name] = inputElem.value;
+                    });
+                updateData(index, productData);
+            }
+            e.preventDefault();
+        });
+    }
+
+    function deleteData(index) {
+        $.ajax({
+            url: "/api/product/" + index,
+            type: "DELETE",
+            success: getData
+        });
+    }
+
+    function updateData(index, productData) {
+        $.ajax({
+            url: "/api/product/" + index,
+            type: "PUT",
+            data: productData,
+            success: getData,
+            error: function (jqXHR, status, error) {
+                var errorRow = $("button[data-id=" + index + "]").closest('tr');
+                errorRow.find("*").addClass("error");
+                var errData = JSON.parse(jqXHR.responseText);
+                for (var i = 0; i < errData.length; i++) {
+                    errorRow.after("<tr class='errMsg error'><td colspan=3>"
+                         + errData[i] + "</td></tr>");
+                }
+            }
+        });
+    }
+
+    $(document).ready(function () {
+        getData();
+    });
+
+})();
